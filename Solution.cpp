dc6543e47c3a03bf4953939d19d38fe573d13c45
26sam <60804996+26sam@users.noreply.github.com>
2021-09-01 16:01:25 +0530

    Add files via upload

---
+++ Solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void init()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	#endif // ONLINE_JUDGE
+}
+
+int main()
+{
+	init();
+	int a,b;cin>>a>>b;
+	cout << a+b << endl;
+	return 0;
+}
